aa50335f4260fbec893432c35b43614059bbb746
petrakos <contactpetrosk@gmail.com>
2023-09-04 16:22:59 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple tax calculation app.
---
+++ src/AfterTax.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AfterTax extends Application {
+
+    
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+
+
+
+
+
+        
+    }
+}
