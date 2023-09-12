d16f495023ca263188fcd8f4043c03de09df6bcd
petrakos <contactpetrosk@gmail.com>
2023-09-12 22:36:19 +0300

    Add test cases for Tax class

--- src/Tax.java
+++ src/Tax.java
@@ -45,6 +45,8 @@ public class Tax {
         taxAmount += remainingAmount * taxPercentage[taxScale];
         return taxAmount;
 
+        
+
     }
     
 }
---
+++ src/TaxTests.java
@@ -0,0 +1,35 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+
+public class TaxTests {
+    
+    private static final double MAX_INSURANCE_CONTRIBUTION_AMOUNT = 7126.95;
+    private static final double INSURANCE_CONTRIBUTION_PERCENTAGE = 0.13867;
+    private static final double MIN_GROSS_AMT = 100.0;
+    double grossAmountThreshold = new BigDecimal(Double.toString(MAX_INSURANCE_CONTRIBUTION_AMOUNT / INSURANCE_CONTRIBUTION_PERCENTAGE)).setScale(2, RoundingMode.HALF_UP).doubleValue();
+
+
+    /**
+     * This method tests for cases where the gross income should produce tax insurance contribution that is less than the maximum amount.
+    */
+    @Test
+    public void testForAmountsThatProduceLessThanMaxInsuranceContribution () {
+        double testAmount = Math.random() * (grossAmountThreshold - MIN_GROSS_AMT) + MIN_GROSS_AMT;
+        Assertions.assertTrue(Tax.caclculateAnnualInsuranceContributions(testAmount) < MAX_INSURANCE_CONTRIBUTION_AMOUNT);
+    }
+
+    /**
+     * This method tests for cases where the gross income should produce tax insurance contribution that is equal to the maximum amount.
+    */
+    @Test
+    public void testForAmountsThatProduceMoreThanMaxInsuranceContribution () {
+        double testAmount = Math.random() * grossAmountThreshold + grossAmountThreshold + 1;
+        Assertions.assertEquals(Tax.caclculateAnnualInsuranceContributions(testAmount), MAX_INSURANCE_CONTRIBUTION_AMOUNT);
+    }
+}
